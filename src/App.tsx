7a55dc6e6160b14f51b43554451c800d7bceaff2
천승민 <cheon.sm@gsretail.com>
2022-03-23 15:07:48 +0900

    feat: add column renderer in toast Grid and React

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React, { useState } from "react";
-import "./App.css";
-import axios from "axios";
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import 'tui-grid/dist/tui-grid.css';
+import Grid from '@toast-ui/react-grid';
+import { Button } from '@mui/material';
+
+function MuiButton() {
+	return <Button>MuiButton</Button>;
+}
+
+class RendererTest {
+	el: HTMLDivElement;
+	constructor(props: any) {
+		this.el = document.createElement('div');
+
+		this.render();
+	}
+
+	getElement() {
+		return this.el;
+	}
+
+	render() {
+		ReactDOM.render(<MuiButton />, this.el);
+	}
+}
 
 function App() {
-  const postRequest = () => {
-    const dateCheck = new Date();
-    axios.post("postUrl", {
-      dateCheck,
-    });
-  };
-
-  return (
-    <div>
-      <button
-        onClick={() => {
-          postRequest();
-        }}
-      >
-        post request
-      </button>
-    </div>
-  );
+	const [columns] = useState([
+		{
+			header: 'NO.',
+			name: 'number',
+			renderer: {
+				type: RendererTest,
+			},
+		},
+		{
+			header: 'name',
+			name: 'name',
+		},
+	]);
+	const [rowData] = useState([
+		{
+			number: '1',
+			name: 'foo',
+		},
+		{
+			number: '2',
+			name: 'bar',
+		},
+	]);
+	return (
+		<div>
+			<Grid columns={columns} data={rowData} />
+			<MuiButton />
+		</div>
+	);
 }
 
 export default App;

6933693a1e34519d4f5e6edc4a442a7ba80cfcc2
천승민 <cheon.sm@gsretail.com>
2022-03-29 17:16:49 +0900

    if you request with Date object param, UTC format will be requested. so if you want request with ko timezone, should use date-fns format

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
-import { Typography } from '@mui/material';
+import React from 'react';
+import axios from 'axios';
+import { format } from 'date-fns';
 
 function App() {
+	const date = new Date('2015-10-12 12:00:00');
+
+	console.log(date.toJSON());
 	return (
 		<>
-			<Typography component={'span'} display="inline">
-				<div>test</div>
-			</Typography>
+			<button
+				onClick={() => {
+					const dateFormatting = format(date, 'yyyy-MM-dd hh:mm');
+					const params = {
+						dateFormatting: dateFormatting,
+					};
+					axios.post('url', params);
+				}}
+			>
+				date param test!
+			</button>
 		</>
 	);
 }
